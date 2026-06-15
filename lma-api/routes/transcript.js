db56e1bc71a368b59cfe427d667b370929ca0442
nddev15 <lewisvn1234@gmail.com>
2026-06-16 03:45:04 +0700

    Delete lma-api directory

--- lma-api/routes/transcript.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/transcript', require(`${process.cwd()}/controllers/discord/transcript`));
-
-module.exports = router;
